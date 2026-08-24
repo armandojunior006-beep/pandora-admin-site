777fc89441466add8e2fdced43db79428973b021
Pandora Bot Admin <armandojunior006@gmail.com>
2026-08-23 21:26:25 -0300

    chore: DOWNLOAD_URL -> SpiderBot-Setup-2.0.12

--- app/landing/constants.js
+++ app/landing/constants.js
@@ -2,4 +2,4 @@
 // passa de 100MB, acima do limite de asset estático do Vercel). Atualize
 // aqui a cada nova versão publicada (ver README/scripts/publish do bot).
 export const DOWNLOAD_URL =
-  "https://github.com/armandojunior006-beep/pandora-admin-site/releases/download/v2.0.10/SpiderBot-Setup-2.0.10.exe";
+  "https://github.com/armandojunior006-beep/pandora-admin-site/releases/download/v2.0.12/SpiderBot-Setup-2.0.12.exe";
